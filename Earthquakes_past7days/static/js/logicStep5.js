2057d70c34d6b65e0543e7e509f420cfdd965bd7
Ekanpat <ekanpat83@gmail.com>
2020-12-20 14:24:23 -0500

    Cleaning of EQ project

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -22,6 +22,14 @@ let baseMaps = {
   "Satellite": satelliteStreets
 };
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
 // Create the map object with the center of our map changed to the geographic center of the United States
 let map = L.map('mapid', {
     center: [39.5, -98.5],
@@ -29,9 +37,9 @@ let map = L.map('mapid', {
     layers: [streets]
 })
 
-
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // // Accessing the airport GeoJSON URL
 // let torontoHoods = "https://raw.githubusercontent.com/Ekanpat/Mapping_Earthquakes/main/torontoNeighborhoods.json";
@@ -45,19 +53,40 @@ let myStyle = {
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     console.log(data);
         // This function returns the style data for each of the earthquakes we plot on
-// the map. We pass the magnitude of the earthquake into two separate functions
-// to calculate the color and radius.
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
     function styleInfo(feature) {
       return {
         opacity: 1,
         fillOpacity: 1,
-        fillColor: getColor(feature.properties.mag),
+        fillColor: "#ffae42",
         color: "#000000",
-        radius: getRadius(feature.properties.mag),
+        radius: getRadius(),
         stroke: true,
         weight: 0.5
       };
     }
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+
+    function getColor(magnitude) {
+      if (magnitude > 5) {
+        return "#ea2c2c";
+      }
+      if (magnitude > 4) {
+        return "#ea822c";
+      }
+      if (magnitude > 3) {
+        return "#ee9c00";
+      }
+      if (magnitude > 2) {
+        return "#eecc00";
+      }
+      if (magnitude > 1) {
+        return "#d4ee00";
+      }
+      return "#98ee00";
+    }
+
     // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
     function getRadius(magnitude) {
@@ -68,15 +97,52 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
       
       // Creating a GeoJSON layer with the retrieved data.
+  // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
-        
-        // We turn each feature into a circleMarker on the map.
-  pointToLayer: function(_feature, latlng) {
+    // We turn each feature into a circleMarker on the map.
+      pointToLayer: function(feature, latlng) {
           console.log(data);
           return L.circleMarker(latlng);
         },
-          // We set the style for each circleMarker using our styleInfo function.
-        style: styleInfo
-      }).addTo(map);
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+      onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+  }).addTo(earthquakes);
+
+//Then we add the earthquake layer to our map
+    earthquakes.addTo(map);
+
 });
 
+// Create a legend control object.
+  var legend = L.control({
+    position: 'bottomright'
+  });
+  // Then add all the details for the legend.
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+  // };
+  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const colors = [
+    "#98ee00",
+    "#d4ee00",
+    "#eecc00",
+    "#ee9c00",
+    "#ea822c",
+    "#ea2c2c"
+  ];
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < magnitudes.length; i++) {
+     console.log(colors[i]);
+     div.innerHTML +=
+       "<i style='background: " + colors[i] + "'></i> " +
+       magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+  }
+   return div;
+ };
+
+legend.addTo(map); 
